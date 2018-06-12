95885d2b65221aaeb76757a888b1fd0e1c20429a
Brian Mattison <mizzioninc@gmail.com>
2018-06-12 11:46:24 -0700

    Updated for 1.3 beta
    I have updated this to work for 1.3 beta

--- AutoGate/AutoGate/ModEntry.cs
+++ AutoGate/AutoGate/ModEntry.cs
@@ -1,97 +1,74 @@
-﻿using StardewModdingAPI;
+﻿using Microsoft.Xna.Framework;
+using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
-using Microsoft.Xna.Framework;
+using StardewValley.Locations;
+using StardewValley.Objects;
+using StardewValley.Network;
+using SObject = StardewValley.Object;
 using System;
+using System.Collections.Generic;
 
-namespace ModEntry
+namespace AutoGate
 {
-	/// <summary>The mod entry point.</summary>
-	public class ModEntry : Mod
-	{
+    public class ModEntry : Mod
+    {
+        public SerializableDictionary<Vector2, SObject> gateList;
 
-		//The Storage Map for the gate objects, Vector2 is the position of the gate, bool is if it is open (true -> isOpen, false -> isClosed)
-		public SerializableDictionary<Vector2, bool> gateList = new SerializableDictionary<Vector2, bool>();
+        public override void Entry(IModHelper helper)
+        {
+            PlayerEvents.Warped += this.EnteredNewLocation;
+            LocationEvents.ObjectsChanged += this.CreatedOrDestroyedGate;
+            GameEvents.HalfSecondTick += this.ReceiveHalfSecondTick;
+        }
 
-		/*********
-		** Public methods
-		*********/
-		/// <summary>Initialise the mod.</summary>
-		/// <param name="helper">Provides methods for interacting with the mod directory, such as read/writing a config file or custom JSON files.</param>
-		public override void Entry(IModHelper helper)
-		{
-			LocationEvents.CurrentLocationChanged += this.EnteredNewLocation;
-			LocationEvents.LocationObjectsChanged += this.CreatedOrDestroyedGate;
-			GameEvents.HalfSecondTick += this.ReceiveHalfSecondTick;
-		}
+        private void EnteredNewLocation(object sender, EventArgsPlayerWarped e)
+        {
+            this.gateList = new SerializableDictionary<Vector2, StardewValley.Object>();
+            OverlaidDictionary<Vector2, SObject> objects = Game1.currentLocation.objects;
+            foreach (Vector2 key in objects.Keys)
+            {
+                if (objects[key].name.Equals("Gate"))
+                {
+                    this.gateList.Add(key, objects[key]);
+                    //this.Monitor.Log(string.Format("{0}", (object)key.ToString()), (LogLevel)1);
+                }
+            }
+        }
 
+        private void CreatedOrDestroyedGate(object sender, EventArgsLocationObjectsChanged e)
+        {
+            this.gateList = new SerializableDictionary<Vector2, StardewValley.Object>();
+            OverlaidDictionary<Vector2, SObject> objects = Game1.currentLocation.objects;
+            
+            foreach (Vector2 key in objects.Keys)
+            {
+                if (objects[key].name.Equals("Gate"))
+                {
+                    this.gateList.Add(key, objects[key]);
+                    //this.Monitor.Log(string.Format("{0}", (object)key.ToString()), (LogLevel)1);
+                }
+            }
+        }
 
-		/*********
-		** Private methods
-		*********/
-		/// <summary>The method invoked when the player presses a keyboard button.</summary>
-		/// <param name="sender">The event sender.</param>
-		/// <param name="e">The event data.</param>
-		private void ReceiveKeyPress(object sender, EventArgsKeyPressed e)
-		{
-			this.Monitor.Log($"Player pressed {e.KeyPressed}.");
-		} 
-			{
-				if (objectList[key] is StardewValley.Fence)
-				{
-					gateList.Add(key, false);
-					if (Game1.currentLocation.objects[key].isPassable())
-					{
-						Game1.currentLocation.objects[key].checkForAction(Game1.player);
-	}
-}
-			}
-		}
-
-		private void CreatedOrDestroyedGate(object sender, EventArgsLocationObjectsChanged e)
-{
-
-	gateList = new SerializableDictionary<Vector2, bool>();
-	SerializableDictionary<Vector2, StardewValley.Object> objectList = Game1.currentLocation.objects;
-	foreach (Vector2 key in objectList.Keys)
-	{
-		if (objectList[key] is StardewValley.Fence)
-		{
-			gateList.Add(key, false);
-			if (Game1.currentLocation.objects[key].isPassable())
-			{
-				Game1.currentLocation.objects[key].checkForAction(Game1.player);
-			}
-		}
-	}
-}
-
-private void ReceiveHalfSecondTick(object sender, EventArgs e)
-{
-	Vector2[] adjPos = Utility.getAdjacentTileLocations(Game1.player.getTileLocation()).ToArray();
-
-	foreach (Vector2 gateKey in gateList.Keys)
-	{
-		bool nextToGate = false;
-		foreach (Vector2 position in adjPos)
-		{
-			if (gateKey.Equals(position) || gateKey.Equals(Game1.player.position))
-			{
-				nextToGate = true;
-			}
-		}
-
-		if (nextToGate && gateList[gateKey] == false)
-		{
-			Game1.currentLocation.objects[gateKey].checkForAction(Game1.player);
-			gateList[gateKey] = true;
-		}
-		else if (!nextToGate && gateList[gateKey] == true)
-		{
-			Game1.currentLocation.objects[gateKey].checkForAction(Game1.player);
-			gateList[gateKey] = false;
-		}
-	}
-}
-	}
+        private void ReceiveHalfSecondTick(object sender, EventArgs e)
+        {
+            if (!Context.IsWorldReady || gateList == null)
+                return;
+            Vector2[] array = Utility.getAdjacentTileLocations(Game1.player.getTileLocation()).ToArray();
+            foreach (Vector2 key in this.gateList.Keys)
+            {
+                bool flag = false;
+                foreach (Vector2 other in array)
+                {
+                    if (key.Equals(other) && !(Game1.currentLocation.objects)[key].isPassable())
+                        flag = true;
+                }
+                if (flag)
+                    Game1.currentLocation.objects[key].checkForAction(Game1.player, false);
+                else if ((Game1.currentLocation.objects)[key].isPassable())
+                    Game1.currentLocation.objects[key].checkForAction(Game1.player, false);
+            }
+        }
+    }
 }
